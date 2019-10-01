9cc449b35a1878af20ace75904ab11990a2c9526
krishnaw14 <krishnaw14@gmail.com>
2019-10-01 16:21:11 +0530

    Latent Traversal

--- trainers/classifier_vae.py
+++ trainers/classifier_vae.py
@@ -4,6 +4,7 @@ from misc.factor_vae import kl_divergence, permute_dims, get_data
 from tqdm import tqdm
 import os
 import numpy as np 
+import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
@@ -17,27 +18,62 @@ class ClassifierVAETrainer(Trainer):
 	def __init__(self, model, classifier, config, device):
 		super(ClassifierVAETrainer, self).__init__(model, config, device)
 
-		self.gamma = config['model']['gamma']
+		self.gamma_init = config['model']['gamma']
+		self.gamma_update_rate = config['model']['gamma_update_rate']
 		self.device = device
-		self.classifier_loss = nn.CrossEntropyLoss().cuda()
+		self.recon_loss = nn.MSELoss().cuda()
 		self.classifier = classifier
 		self.optim_classifier = self.get_optimizer(classifier.parameters(), config['train']['optim'], config['train']['c_lr'])
 
-	def forward_pass(self, img, label):
-		img_recon, mu, logvar, z = self.model(img)
-		recon_loss = torch.mean((img_recon-img)**2)
+		self.lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=[10,20], gamma=0.5)
+		self.identity = torch.eye(10).cuda()
+
+	def update_gamma(self, recon_loss):
+		self.gamma = self.gamma_init
+		self.gamma = self.gamma_init - self.gamma_update_rate*recon_loss
+
+	def latent_traversal(self):
+		self.model.eval()
+		sample_idx = 15
+		interpolation_vals = torch.arange(-3,3, 2/3)
+		img, y = self.val_loader.dataset.__getitem__(sample_idx)
+		img = img.unsqueeze(0)
+		y = self.identity[y].unsqueeze(0)
+		img = img.to(self.device)
+		img_z = self.model.encoder(img)[:, :self.model.z_dim]
+
+		samples = []
+		for row in range(self.model.z_dim):
+			z_ = img_z.clone()
+			for val in interpolation_vals:
+				z_[:, row] = val
+				sample = self.model.decoder(z_ + self.model.label_layer(y).unsqueeze(-1).unsqueeze(-1))
+				samples.append(sample)
+		samples = torch.cat(samples, dim=0).cpu()
+
+		save_image(samples, os.path.join(self.log_result_dir, 'Traverse_{}.png'.format(sample_idx)), nrow=9, normalize=True)
+
+
+	def forward_pass(self, img, label, classify=True):
+		img_recon, mu, logvar, z = self.model(img, self.identity[label])
+		# recon_loss = torch.mean((img_recon-img)**2)
+		# recon_loss = F.binary_cross_entropy_with_logits(img_recon, img, size_average=False).div(img.size(0))
+		recon_loss = self.recon_loss(img_recon, img)
 		kld = kl_divergence(mu, logvar)
 
 		vae_loss = recon_loss + kld
 
-		Cz = self.classifier(z)
-		classifier_loss = self.classifier_loss(Cz, label)
+		# if classify:
+		# 	# Cz = self.classifier(z)
+		# 	classifier_loss = self.classifier_loss(Cz, label)
+		# else:
+		# 	classifier_loss = 0
 
-		total_loss = recon_loss + kld + self.gamma*classifier_loss
+		# total_loss = recon_loss + kld + self.gamma*classifier_loss
 
 		# import pdb; pdb.set_trace()
 
-		return total_loss, recon_loss, vae_loss, classifier_loss, img_recon
+		return recon_loss, vae_loss, img_recon, z
 
 	def train(self):
 		for epoch in range(self.num_epochs):
@@ -47,32 +83,48 @@ class ClassifierVAETrainer(Trainer):
 				img, label = data
 				img, label = img.to(self.device), label.to(self.device)
 
-				total_loss, recon_loss, vae_loss, classifier_loss, img_recon = self.forward_pass(img, label)
+				if epoch == 0:
+					classify = True
+				else:
+					classify = True
+				recon_loss, vae_loss, img_recon, z = self.forward_pass(img, label, classify)
 				if epoch%self.log_result_step == 0 and nbatch == 0:
 					self.save_results(epoch, img_recon)
+
+				# self.update_gamma(recon_loss.item())
+				# classifier_loss = self.gamma*classifier_loss
+				total_loss = vae_loss #+ self.gamma*classifier_loss
+
 				self.optimizer.zero_grad()
-				vae_loss.backward(retain_graph=True)
+				vae_loss.backward(retain_graph=False)
 				self.optimizer.step()
 
-				self.optim_classifier.zero_grad()
-				classifier_loss.backward(retain_graph=False)
-				self.optim_classifier.step()
+				# if classify:
+				# 	self.optim_classifier.zero_grad()
+				# 	classifier_loss.backward(retain_graph=False)
+				# 	self.optim_classifier.step()
 
 				epoch_loss += total_loss
 
-				pbar.set_description('Epoch: {}, Total Loss: {}, Classifier Loss {}, Recon Error: {}'.format(epoch, 
-					total_loss.item(), classifier_loss.item(), recon_loss.item()))
+				pbar.set_description('Epoch: {}, Total Loss: {}, Classifier Loss {}, Recon Error: {}, gamma: {}'.format(epoch, 
+					total_loss.item(), 0, recon_loss.item(), 0))
+				# import pdb; pdb.set_trace()
+			# self.lr_scheduler.step()
+				
 				
 			avg_epoch_loss = epoch_loss/(self.iters_per_epoch)
 
 			print('Average Loss:', avg_epoch_loss.item())
 			print('Reconstruction Loss', recon_loss.item())
-			print('Classifier Loss:', classifier_loss.item())
+			# print('Classifier Loss:', classifier_loss)
 
 			if epoch%self.val_step == 0:
 				self.validate()
 			if epoch%self.save_param_step == 0:
 				self.save_checkpoint(epoch, avg_epoch_loss, is_best=False)
+
+		# import pdb; pdb.set_trace();
+		self.latent_traversal()
 
 
 	def validate(self):
@@ -80,3 +132,4 @@ class ClassifierVAETrainer(Trainer):
 
 	def save_results(self, epoch, img_recon):
 		save_image(img_recon, os.path.join(self.log_result_dir, 'epoch_{}.png'.format(epoch)), nrow=8, normalize=True)
+
